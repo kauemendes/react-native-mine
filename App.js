1baca7b5b3f1b6ceb9cf6818e3b3ca033ea8a346
Kaue Mendes <kaue.mendes@gmail.com>
2019-03-08 12:16:47 -0300

    React Native Mine Board Logic

---
+++ App.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import params from './src/Params'
+import Field from './src/components/Field'
+import MineField from './src/components/MineField'
+
+import { createMinedBoard } from './src/functions'
+
+const board = createMinedBoard()
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: createMinedBoard(cols, rows, this.minesAmount())
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mines</Text>
+        <Text style={styles.instructions}>Tamanho do Tabuleiro:
+          {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+        <View style={styles.board}>
+          <MineField board={this.state.board} />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#AAA'
+  }
+});
